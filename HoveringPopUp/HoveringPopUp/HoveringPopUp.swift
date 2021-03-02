4c9bfc876baed6d2931a0a284b5c90c950ee8c9a
Arman Zoghi <shadowofarman@hotmail.com>
2021-03-02 12:43:10 +0330

    Added a function to find the main window currently active

--- HoveringPopUp/HoveringPopUp/HoveringPopUp.swift
+++ HoveringPopUp/HoveringPopUp/HoveringPopUp.swift
@@ -17,13 +17,17 @@ open class HoveringPopUp: UIView {
     
     //MARK: ----- Variables -----
     
-    open var compactTitleString: String? {
+    /// Sets the text to show in the title when in compact mode.
+    open var compactTitleText: String? {
         didSet {
-            
+            compactLabel.text = self.compactTitleText ?? "POP"
         }
     }
     
+    /// The constraints used for the popUpView
     fileprivate var viewConstraints : [NSLayoutConstraint] = []
+    
+    fileprivate var mainWindow: UIWindow?
     
     //MARK: - View Lifecycle
     
@@ -43,8 +47,50 @@ open class HoveringPopUp: UIView {
         
     }
     
+    /// Finds the main window currently active.
+    fileprivate func findMainWindow() {
+        if #available(iOS 13.0, *) {
+            if let window = UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .map({$0 as? UIWindowScene})
+                .compactMap({$0})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first {
+                self.mainWindow = window
+            } else {
+                if let window = UIApplication.shared.keyWindow {
+                    self.mainWindow = window
+                }
+            }
+        } else {
+            if let window = UIApplication.shared.keyWindow {
+                self.mainWindow = window
+            }
+        }
+    }
+    
+    //MARK: - Prep
+    
+    /// Prepares the popUpViewToShow
+    /// - Parameters:
+    ///   - fullView: Sets the view to show when in full screen.
+    ///   - title: Sets the title to show when in compact mode.
+    ///   - font: Sets the Font to use for the title inside the compact mode.
+    ///   - backgroundColor: Sets the background color to use for the compact mode.
+    ///   - textColor: Sets the color of the title in compact mode.
+    ///   - shadowColor: Sets the shadow color of the pop up.
+    ///   - borderWidth: Sets the border width of the pop up view.
+    ///   - borderColor: Sets the border color of the pop up view.
+    open func preparePopUp(fullView: UIView, title: String, font: UIFont?, backgroundColor: UIColor?, textColor: UIColor?, shadowColor: UIColor?, borderWidth: CGFloat?, borderColor: UIColor?) {
+
+        self.findMainWindow()
+        
+    }
+    
     //MARK: - Pop Up
     
+    /// Pops up!
+    /// - Parameter direction: Sets the direction to display to pop up from.
     open func popFrom(direction: HoveringPopUpDirection) {
         
     }
